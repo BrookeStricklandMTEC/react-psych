ec6d508d93538a2d3b59e79de31d9e26dd9f10a0
Brooke Strickland <brooke.strickland2904@stu.mtec.edu>
2022-11-21 10:23:06 -0700

    restart and quit button begun

--- react-psych/src/Restart.js
+++ react-psych/src/Restart.js
@@ -1,11 +1,19 @@
 import React from 'react'; 
 import './App.css'
 
-export default function Restart (){
+export default function Restart (){ 
+    
+    const restartFunction = event => {
+    //  this.back()
+    //  or
+    // .reload()
+    };
+
    
     return (
+
         <div>
-            <button id="restart" className="button">
+            <button id="restart" className="button" onClick={restartFunction}>
                 Restart
             </button>
         </div>
